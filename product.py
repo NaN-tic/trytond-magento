1b5bb0c870806120f5d2c269ae5b65aee4a6ef20
resteve <resteve@zikzakmedia.com>
2014-05-08 12:12:14 +0200

    Create product: Get sale shop and taxes/price in external methods

--- product.py
+++ product.py
@@ -46,6 +46,74 @@ class Product:
         return vals
 
     @classmethod
+    def magento_product_esale_saleshops(self, app, product_info):
+        '''
+        Get sale shops (websites)
+        :param app: object
+        :product_info: dict
+        return shops (list)
+        '''
+        pool = Pool()
+        MagentoWebsite = pool.get('magento.website')
+        MagentoExternalReferential = pool.get('magento.external.referential')
+
+        shops = []
+        websites = []
+        for website in product_info.get('websites'):
+            website_ref = MagentoExternalReferential.get_mgn2try(app, 
+            'magento.website', website)
+            websites.append(website_ref.try_id)
+        if websites:
+            magento_websites = MagentoWebsite.browse(websites)
+            for website in magento_websites:
+                shops.append(website.sale_shop[0].id)
+
+        return shops
+
+    @classmethod
+    def magento_product_esale_taxes(self, app, product_info, tax_include=False):
+        '''
+        Get customer taxes and list price and cost price (with or without tax)
+        :param app: object
+        :product_info: dict
+        return customer_taxes (list), list_price, cost_price
+        '''
+        pool = Pool()
+        MagentoTax = pool.get('magento.tax')
+
+        customer_taxes = []
+        list_price = None
+        cost_price = None
+
+        tax_id = product_info.get('tax_class_id')
+        if tax_id:
+            taxs = MagentoTax.search([
+                ('magento_app', '=', app.id),
+                ('tax_id', '=', tax_id),
+                ], limit=1)
+            if taxs:
+                customer_taxes.append(taxs[0].tax.id)
+                if tax_include:
+                    price = product_info.get('list_price')
+                    rate = taxs[0].tax.rate
+                    base_price = base_price_without_tax(price, rate)
+                    list_price = base_price
+                    cost_price = base_price
+
+        if not customer_taxes and app.default_taxes:
+            for tax in app.default_taxes:
+                customer_taxes.append(tax.id)
+            if tax_include:
+                # Get first tax to get base price -not all default taxes-
+                price = Decimal(product_info.get('price'))
+                rate = app.default_taxes[0].rate
+                base_price = base_price_without_tax(price, rate)
+                list_price = base_price
+                cost_price = base_price
+
+        return customer_taxes, list_price, cost_price
+
+    @classmethod
     def create_product_magento(self, shop, code):
         '''
         Get Magento product info and create
@@ -54,10 +122,10 @@ class Product:
         return obj
         '''
         Template = Pool().get('product.template')
-        MagentoWebsite = Pool().get('magento.website')
         MagentoExternalReferential = Pool().get('magento.external.referential')
 
         mgnapp = shop.magento_website.magento_app
+        tax_include = shop.esale_tax_include
 
         store_view = mgnapp.magento_default_storeview or None
         if store_view:
@@ -83,46 +151,17 @@ class Product:
             pvals = self.magento_product_dict2vals(shop, product_info)
 
             #Shops - websites
-            shops = []
-            websites = []
-            for website in product_info.get('websites'):
-                website_ref = MagentoExternalReferential.get_mgn2try(mgnapp, 
-                'magento.website', website)
-                websites.append(website_ref.try_id)
-            if websites:
-                magento_websites = MagentoWebsite.browse(websites)
-                for website in magento_websites:
-                    shops.append(website.sale_shop[0].id)
+            shops = self.magento_product_esale_saleshops(mgnapp, product_info)
             if shops:
                 tvals['esale_saleshops'] = [('add', shops)]
 
-            #Taxes
-            tax = None
-            tax_id = product_info.get('tax_class_id')
-            if tax_id:
-                taxs = Pool().get('magento.tax').search([
-                    ('magento_app', '=', mgnapp.id),
-                    ('tax_id', '=', tax_id),
-                    ], limit=1)
-                if taxs:
-                    tvals['customer_taxes'] = [('add', [taxs[0].tax.id])]
-                    if shop.esale_tax_include:
-                        price = tvals.get('list_price')
-                        rate = taxs[0].tax.rate
-                        base_price = base_price_without_tax(price, rate)
-                        tvals['list_price'] = base_price
-                        tvals['cost_price'] = base_price
-            elif mgnapp.default_taxes:
-                taxs = []
-                for tax in mgnapp.default_taxes:
-                    taxs.append(tax.id)
-                tvals['customer_taxes'] = [('add', taxs)]
-                if shop.esale_tax_include:
-                    # Get first tax to get base price -not all default taxes-
-                    price = tvals.get('list_price')
-                    rate = mgnapp.default_taxes[0].rate
-                    base_price = base_price_without_tax(price, rate)
-                    tvals['list_price'] = base_price
-                    tvals['cost_price'] = base_price
+            #Taxes and list price and cost price with or without taxes
+            customer_taxes, list_price, cost_price = self.magento_product_esale_taxes(mgnapp, product_info, tax_include)
+            if customer_taxes:
+                tvals['customer_taxes'] = [('add', customer_taxes)]
+            if list_price:
+                tvals['list_price'] = list_price
+            if cost_price:
+                tvals['cost_price'] = cost_price
 
             return Template.create_esale_product(shop, tvals, pvals)
