87ebbe274215c1027d70c01c218b01f8c4ad3707
Raimon Esteve <raimon@nan-tic.com>
2017-01-23 14:18:42 +0100

    Convert state to Magento status when export sales CSV

--- __init__.py
+++ __init__.py
@@ -3,30 +3,32 @@
 # the full copyright notices and license terms.
 
 from trytond.pool import Pool
-from .magento_core import *
-from .magento_referential import *
-from .product import *
-from .shop import *
+from . import magento_core
+from . import magento_referential
+from . import product
+from . import sale
+from . import shop
 
 
 def register():
     Pool.register(
-        MagentoApp,
-        MagentoWebsite,
-        MagentoStoreGroup,
-        MagentoStoreView,
-        MagentoCustomerGroup,
-        MagentoRegion,
-        MagentoAppCustomer,
-        MagentoShopStatus,
-        MagentoAppCustomerMagentoStoreview,
-        MagentoAppCountry,
-        MagentoAppLanguage,
-        MagentoApp2,
-        MagentoStoreGroup2,
-        MagentoExternalReferential,
-        MagentoTax,
-        MagentoAppDefaultTax,
-        Product,
-        SaleShop,
+        magento_core.MagentoApp,
+        magento_core.MagentoWebsite,
+        magento_core.MagentoStoreGroup,
+        magento_core.MagentoStoreView,
+        magento_core.MagentoCustomerGroup,
+        magento_core.MagentoRegion,
+        magento_core.MagentoAppCustomer,
+        magento_core.MagentoShopStatus,
+        magento_core.MagentoAppCustomerMagentoStoreview,
+        magento_core.MagentoAppCountry,
+        magento_core.MagentoAppLanguage,
+        magento_core.MagentoApp2,
+        magento_core.MagentoStoreGroup2,
+        magento_core.MagentoTax,
+        magento_core.MagentoAppDefaultTax,
+        magento_referential.MagentoExternalReferential,
+        product.Product,
+        sale.Sale,
+        shop.SaleShop,
         module='magento', type_='model')
